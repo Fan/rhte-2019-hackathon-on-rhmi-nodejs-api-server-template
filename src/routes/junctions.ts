87e35fb9ce5535e6140e9619a13305a7be02198c
David Fan <davidfalex@gmail.com>
2019-09-26 11:33:58 +0200

    not sure if i did this right lol i love pushing to master

--- src/routes/junctions.ts
+++ src/routes/junctions.ts
@@ -1,9 +1,25 @@
 import * as express from 'express';
+import { query } from '../db';
 
 const junctionsRoute = express.Router();
 
 junctionsRoute.get('/', async (req, res) => {
   res.status(501).end('not implemented');
+});
+
+type JunctionObject = {
+  junction_id: number;
+  count_ns: number;
+  count_ew: number;
+  last_updated: string;
+  longitude: number;
+  latitude: number;
+};
+
+junctionsRoute.get('/', async (req, res) => {
+  const result = await query<JunctionObject>('SELECT * FROM junction_info;');
+
+  res.json(result.rows);
 });
 
 export default junctionsRoute;
